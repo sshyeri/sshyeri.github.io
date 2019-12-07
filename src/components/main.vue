33dd065e278f211d2e36d7fe165b43d5c902f745
sshyeri <sshyeri.k@gmail.com>
2019-12-07 20:58:51 +0900

    add mainpage

---
+++ src/components/main.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="main">
+		<h1>Hyeri's blog</h1>
+
+	</div>
+</template>
+<script>
+export default {
+  name: 'main',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+<style scoped>
+h1{
+  color: #42b983;
+}
+</style>
